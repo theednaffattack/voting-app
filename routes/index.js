fa35aad188a01d2ba19299a5a66ecb0eb9d1aa58
Eddie Naff <eddienaff@gmail.com>
2017-05-31 09:50:10 -0700

    Add  route as test of req.param capture / manipulation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+
 const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const eddie = { name: 'Eddie', age: 100, cool: true };
+  // console.log('Hey');
+  // res.send('Hey! It works!');
+  res.json(eddie);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
