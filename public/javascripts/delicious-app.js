9c6a278b2973af8c31075dda05529641795f076d
Eddie Naff <eddienaff@gmail.com>
2017-06-01 11:42:55 -0700

    Add google map autocomplete to address field

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,8 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+
+// below we're using bling selections to supply the arguments
+// note this is all client-side
+autocomplete($('#address'), $('#lat'), $('#lng'));
